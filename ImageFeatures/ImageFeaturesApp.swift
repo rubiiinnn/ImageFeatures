36b7216ddb420ee3ca00c92822cfc852cf600b7e
Rubin <miss.rubin.8575@gmail.com>
2025-10-16 10:49:49 +0530

    Initial Commit

---
+++ ImageFeatures/ImageFeaturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageFeaturesApp.swift
+//  ImageFeatures
+//
+//  Created by Rubin Kour on 16/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ImageFeaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ImageFeaturesTests/ImageFeaturesTests.swift
@@ -0,0 +1,17 @@
+//
+//  ImageFeaturesTests.swift
+//  ImageFeaturesTests
+//
+//  Created by Rubin Kour on 16/10/25.
+//
+
+import Testing
+@testable import ImageFeatures
+
+struct ImageFeaturesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
