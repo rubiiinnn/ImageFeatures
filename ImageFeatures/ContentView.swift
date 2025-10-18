a310ea6c7ad52596b253550a8b90b925d200948d
Rubin <miss.rubin.8575@gmail.com>
2025-10-18 20:23:34 +0530

    Image creation in swift ui

--- ImageFeatures/ContentView.swift
+++ ImageFeatures/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = ""
+    @State private var imageName = ""
+    @State private var imageNo = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            Spacer()
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 300)
+                .clipShape(Capsule())
+            Button("Show"){
+                let m1 = "great"
+                let m2 = "waow"
+                
+                message = (message == m1 ? m1 : m2)
+                imageName = (imageName == "image0" ? "image1" : "image2")
+                
+                //TODO: Update the imageName variable
+                //imageName = "image" + String(imageNo)
+                imageName = "image\(String(imageNo))"
+                
+                imageNo += 1; // to print from 0-5
+                
+                if imageNo>9{
+                    imageNo = 0
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.blue)
+            
         }
-        .padding()
     }
 }
 
